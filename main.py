477e88350b16820b5f797cd5de3f372711ffcbed
Maxime Moreillon <m.moreillon@gmail.com>
2022-05-03 20:15:53 +0100

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from os import environ
+from dotenv import load_dotenv
+from time import sleep
+
+load_dotenv()
+
+period = 60 * 30
+
+def update():
+
+    HOSTNAME = environ.get('GOOGLE_DOMAINS_HOSTNAME')
+    USERNAME = environ.get('GOOGLE_DOMAINS_USERNAME')
+    PASSWORD = environ.get('GOOGLE_DOMAINS_PASSWORD')
+
+    url = f'https://{USERNAME}:{PASSWORD}@domains.google.com/nic/update?hostname={HOSTNAME}'
+
+    try:
+          
+        response = requests.post(url)
+        print(response.text)
+
+    except:
+
+        print('Update failed')
+
+
+while True:
+    update()
+    sleep(period)
